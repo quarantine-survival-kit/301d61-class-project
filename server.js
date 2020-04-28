61ce8f43e75071d28a29e709edb42d12501cf66c
KentFletcher <kentfletch09@gmail.com>
2020-04-28 14:03:58 -0700

    added constructor and helper function

--- server.js
+++ server.js
@@ -26,6 +26,36 @@ app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
+//constructor function for Movies
+function Movie (movie) {
+  this.title = movie.title;
+  this.overview = movie.overview;
+  this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  this.popularity = movie.popularity;
+  this.release_date = movie.release_date;
+}
+
+app.get('/index', collectMovieData);
+
+function collectMovieData (request, response) {
+  const query = request.query.search_query;
+  const key = process.env.MOVIE_API_KEY;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}`;
+
+  superagent.get(url)
+    .then(movieResponse => {
+      console.log('RESPONSE FROM API');
+      const data = movieResponse.body.results;
+      console.log(data);
+      let movies = [];
+      data.map(item => movies.push(new Movie(item)));
+      response.render('index', { movies: movies});
+
+    })
+
+    .catch(error => errorHandler(error, request, response));
+}
+
 // Connected to database client
 // const dbClient = new pg.Client(process.env.DATABASE_URL);
 // dbClient.connect( error => {
