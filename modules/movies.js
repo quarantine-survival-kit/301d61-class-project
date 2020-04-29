3a09ae693e872f824f32379e551a2f278dedb911
KentFletcher <kentfletch09@gmail.com>
2020-04-29 13:30:45 -0700

    fixed image linke when no movie poster available

--- modules/movies.js
+++ modules/movies.js
@@ -8,7 +8,7 @@ const db = require('./db');
 function Movie (movie) {
   this.title = movie.title;
   this.overview = movie.overview;
-  this.image_url = movie.poster_path !== null ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`: 'https://i.imgur.com/J5LVHEL.jpg';
+  this.image_url = movie.poster_path !== null ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`: 'https://www.auro-3d.com/wp-content/uploads/2016/08/no-poster-available.jpg';
   this.popularity = movie.popularity;
   this.release_date = movie.release_date;
   this.view_link = movie.title ? `https://www.justwatch.com/us/search?q=${movie.title}` : 'No link available.';
