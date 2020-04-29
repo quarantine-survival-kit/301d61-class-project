0914ba78635d204d468c1d09d6d591aa49134ac4
KentFletcher <kentfletch09@gmail.com>
2020-04-29 09:23:32 -0700

    added link to where to view movies

--- modules/movies.js
+++ modules/movies.js
@@ -10,12 +10,13 @@ function Movie (movie) {
   this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
   this.popularity = movie.popularity;
   this.release_date = movie.release_date;
+  this.view_link = movie.title ? `https://www.justwatch.com/us/search?q=${movie.title}` : 'No link available.';
 }
 
 exports.collectMovieData = function(request, response) {
   const query = request.body.movieSearchInput;
   const key = process.env.MOVIE_API_KEY;
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}`;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}&limit=10`;
 
   superagent.get(url)
     .then(movieResponse => {
